77cf6baaeaeb170f108cb9184667b26fb584688d
Shubhika <shubhikaduhan2327@gmail.com>
2026-04-18 00:53:32 +0530

    Add files via upload

---
+++ app-shubhika.js
@@ -0,0 +1,51 @@
+function generate(){
+    let dictionary = "";
+    if(document.getElementById("id1").checked){
+        dictionary += "qwertyuiopasdfghjklzxcvbnm";
+    }
+
+    if(document.getElementById("id2").checked){
+        dictionary += "QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
+
+    if(document.getElementById("id3").checked){
+        dictionary += "1234567890";
+    }
+
+    if(document.getElementById("id4").checked){
+        dictionary += "!@#$%^&*()";
+    }
+    // console.log(dictionary);
+
+    //now we will decide the length of the password it will 
+
+    let passLength = document.querySelector('input[type = "range"]').value;
+
+    if (passLength <1, dictionary.length ===0){
+        return;
+    }
+
+    let password = "";
+    for(i = 0; i <passLength;i++){
+        const pos = Math.floor(Math.random()    *dictionary.length);
+        password += dictionary[pos];
+    }
+
+    document.querySelector('input[type = "text"]').value = password;
+
+    console.log(password);
+}
+
+const rangeInput = document.querySelector(".input-len");
+const span = document.querySelector(".inp span");
+
+rangeInput.addEventListener("input", ()=>{
+    span.textContent = rangeInput.value; 
+})
+
+// console.log(span);
+
+let genPass = document.querySelector(".gen-but");
+genPass.addEventListener("click" , function (){
+    generate(span.innerText);
+})    
---
+++ main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a = 25;
+    int b = 45;
+    int sum = a+b;
+}
